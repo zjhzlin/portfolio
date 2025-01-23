d4b5d55aed50ddaf1397aed12aa0c3c7c843180b
owenmerry <merryowen@gmail.com>
2025-01-23 22:32:19 +0000

    Added fix for tailwind and removed styles

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     react(),     
+    tailwindcss(),
   ],
 })
